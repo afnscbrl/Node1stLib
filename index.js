f24e7ae28583d7dc7975017177a8214fb9f579ed
afnscbrl <afns.cbrl@gmail.com>
2022-03-05 18:49:20 -0300

    refatorado

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import chalk from 'chalk'
 import fs from 'fs'
 
+function extraiLinks(texto) {
+    const regex = /(https?:\/\/[^$#\s]*)>\s([\S]*)<\/a/gm
+    const arrayResultados = []
+    let temp
+    while ((temp = regex.exec(texto))!== null) {
+        arrayResultados.push({[temp[2]]: temp[1]})
+    }
+    return arrayResultados.length === 0 ? 'Não ha links' : arrayResultados
+}
+
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Algo deu ruim!'))
 }
@@ -9,7 +20,7 @@ async function pegaArquivo(caminhoArquivo) {
     const encoding = 'utf-8'
     try {
     const texto = await fs.promises.readFile(caminhoArquivo, encoding)
-    console.log(chalk.green(texto))
+    return extraiLinks(texto)
     } catch(err) {
         trataErro(err)
     }
@@ -24,13 +35,14 @@ async function pegaArquivo(caminhoArquivo) {
 // }
 
 // function pegaArquivo(caminhoArquivo) {
-//     const enconding = 'utf-8';
+//     const enconding = 'utf-8'
 //     fs.readFile(caminhoArquivo, enconding, (err, texto) =>{
 //         if (err) {
-//             trataErro(err);
+//             trataErro(err)
 //         }
 //         console.log(chalk.green(texto))
 //     })
 // }
 
-pegaArquivo('./arquivo/texto1.md');
+//pegaArquivo('./arquivo/texto1.md')
+ export const leArquivo = pegaArquivo
