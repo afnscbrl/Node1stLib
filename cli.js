2a583d78c049f388279626238f7920a48c9404fd
afnscbrl <afns.cbrl@gmail.com>
2022-03-06 10:32:26 -0300

    refatorando

--- cli.js
+++ cli.js
@@ -1,14 +1,18 @@
-import {leArquivo} from './index.js'
+import pegaArquivo from './index.js'
 import chalk from 'chalk'
+import validaURLs from './http-validacao.js'
 
-const caminho = process.argv
+const entrada = process.argv
 
 //console.log(caminho)
 
 async function processaTexto(caminhoDeArquivo) {
-    const resultado = await leArquivo(caminhoDeArquivo[2])
-    console.log(chalk.yellow('lista de links'), resultado)
+    const resultado = await pegaArquivo(caminhoDeArquivo[2])
+    if (caminhoDeArquivo[3] === 'validar') {
+        console.log(chalk.yellow('links validados'), await validaURLs(resultado))
+    } else {console.log(chalk.yellow('lista de links'), resultado)}
+    
 }
 
-processaTexto(caminho)
+processaTexto(entrada)
 
