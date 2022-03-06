7493b45716a571e45962fbb17a323c48fc6b5f66
afnscbrl <afns.cbrl@gmail.com>
2022-03-06 10:32:42 -0300

    refatorando

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import chalk from 'chalk'
 import fs from 'fs'
 
 function extraiLinks(texto) {
-    const regex = /(https?:\/\/[^$#\s]*)>\s([\S]*)<\/a/gm
+    const regex = /(https?:\/\/[^$#\s]*[\w])">\s([\S]*)<\/a/gm
     const arrayResultados = []
     let temp
     while ((temp = regex.exec(texto))!== null) {
@@ -16,7 +16,7 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Algo deu ruim!'))
 }
 
-async function pegaArquivo(caminhoArquivo) {
+export default async function pegaArquivo(caminhoArquivo) {
     const encoding = 'utf-8'
     try {
     const texto = await fs.promises.readFile(caminhoArquivo, encoding)
@@ -26,23 +26,3 @@ async function pegaArquivo(caminhoArquivo) {
     }
 }
 
-// function pegaArquivo(caminhoArquivo) {
-//     const enconding = 'utf-8'
-//     fs.promises
-//     .readFile(caminhoArquivo, enconding)
-//     .then((texto) => console.log(chalk.green(texto)))
-//     .catch((err) => trataErro(err))
-// }
-
-// function pegaArquivo(caminhoArquivo) {
-//     const enconding = 'utf-8'
-//     fs.readFile(caminhoArquivo, enconding, (err, texto) =>{
-//         if (err) {
-//             trataErro(err)
-//         }
-//         console.log(chalk.green(texto))
-//     })
-// }
-
-//pegaArquivo('./arquivo/texto1.md')
- export const leArquivo = pegaArquivo
